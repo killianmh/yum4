3a07dd61ab950c51bdf7c7c9b3b0741e54f57f33
killianmh <killianmh@gmail.com>
2020-10-19 16:59:42 -0700

    styled header for desktop; added useBreakpoint function and BreakpointContext to provide window size context to components; cleaned up components

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,60 @@ import {
   Link
 } from "react-router-dom";
 import '../styles/Header.css';
+import Menu from './Menu'
+
+import {useBreakpoint} from '../contextProviders/BreakpointProvider'
 
 const Header = props => {
+  const breakpoints = useBreakpoint()
+  console.log(breakpoints.desktop)
+
   const searchActive = props.searchActive
+
+  let headerClass = "header"
+  let right
+  let links
+  if (breakpoints.desktop) {
+    headerClass += ' desktop'
+    links = [
+      {
+        path: "/",
+        name: "Home"
+      },
+      {
+        path: "/about",
+        name: "About"
+      },
+    ]
+    right = 
+    <div className="menuCont">
+      <Menu links={links} />
+      <button className="btn alt">Login</button>
+    </div>
+  } else {
+    links = [
+      {
+        path: "/",
+        name: "Home"
+      },
+      {
+        path: "/login",
+        name: "Login"
+      },
+      {
+        path: "/about",
+        name: "About"
+      },
+    ]
+    right = <Menu links={links} />
+  }
   return (
-    <div className="header">
+    <div className={headerClass}>
       <div className="container">
         <div className="title">
           Yum
         </div>
-        {props.children}
+        {right}
       </div>
     </div>
   )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,20 @@ import React from 'react';
 import MenuLink from './MenuLink'
 import '../styles/Nav.css';
 
+import {useBreakpoint} from '../contextProviders/BreakpointProvider'
+
 const Nav = props => {
+  const breakpoints = useBreakpoint()
+  console.log(breakpoints.desktop)
+  let button
+  let navClass = "nav"
+  let ulClass = "navUl"
+  if (!breakpoints.desktop) {
+    button = <button className="closeMenu"><i className="fas fa-times"></i> </button>
+  } else {
+    navClass += ' desktop'
+    ulClass += ' desktop'
+  }
   const links = []
   props.links.forEach((link) => {
     links.push(
@@ -14,9 +27,9 @@ const Nav = props => {
     )
   })
   return(
-    <div className="nav">
-      <button className="closeMenu"><i className="fas fa-times"></i> </button>
-      <ul className="navUl">
+    <div className={navClass}>
+      {button}
+      <ul className={ulClass}>
         {links}
       </ul>
     </div>
